3ee3c4f75ae4d42e5f3f82f65b23bed393f730b4
Juan Gomez <juanmiguelgomez8@gmail.com>
2019-10-16 11:47:00 -0500

    Updating state typings

--- packages/state/types/index.d.ts
+++ packages/state/types/index.d.ts
@@ -1,9 +1,22 @@
 declare module "@juan-utils/state" {
   export type Getter<A = any> = () => A;
   export type Setter<A = any> = (nextState: A) => void;
+  export type EmitEvent<A = any> = (event: any) => void;
+
   export type State = {
-    getState: Getter;
-    setState: Setter;
+    id: any;
+    transitions: Transition[];
+    find: (f: (transition: any) => boolean) => any;
+  };
+
+  export type Transition = {
+    id: any;
+  };
+
+  export type StateData = {
+    id: any;
+    transitions: Transition[];
+    data: any;
   };
 
   export type ObservableConfig = {
@@ -14,7 +27,26 @@ declare module "@juan-utils/state" {
     mapSet: (state: any) => any;
   };
 
+  export type MachineConfig = {
+    states: StateData[];
+    initial: number;
+    onChange: (next: State) => void;
+    comparer: (a: Transition, b: Transition) => boolean;
+  };
+
   export function useState(initialValue: any): [Getter, Setter];
-  export function createState(initialValue: any): State;
   export function useObservable(config: ObservableConfig): [Getter, Setter];
+
+  export function compare(a: any, b: any): boolean;
+  export function compare(a: any): (b: any) => boolean;
+  export function State(id: any, transition: any[], data: any): State;
+  export function useMachine(config: MachineConfig): [Getter, EmitEvent];
+
+  export function useTimeline<State = any>(
+    onEvent: (event: any, past: State[]) => State
+  ): [Getter<State[]>, EmitEvent];
+
+  export function useTimemachine(
+    config: MachineConfig
+  ): [Getter<any[]>, EmitEvent];
 }
