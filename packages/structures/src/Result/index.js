2a48916cb4f66923dce548eb3bf190a5413b8fef
Juan Gomez <juanmiguelgomez8@gmail.com>
2020-01-31 15:36:09 -0500

    Adding Result ADT

---
+++ packages/structures/src/Result/index.js
@@ -0,0 +1,32 @@
+const _Error = (value) => {
+    return {
+        get __class__(){
+            return "Error"
+        },
+        map: (f) => _Error(value),
+        get: () => value,
+        isOk: () => false,
+        isError: () => true,
+        onError: (f) => f(value),
+    }
+}
+
+const Ok = (value) => {
+    return {
+        get __class__(){
+            return "Ok"
+        },
+        map: (f) => Ok(f(value)),
+        get: () => value,
+        isOk: () => true,
+        isError: () => false,
+        onError: (f) => value,
+    }
+}
+
+const Result = {
+    Error: _Error,
+    Ok
+}
+
+export { Result }
