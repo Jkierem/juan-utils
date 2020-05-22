71001e91f0c716033d8f001b519cc77bbee66839
jkierem <juanmiguelgomez8@gmail.com>
2020-05-22 09:10:43 -0500

    Removing esm and umd builds

--- packages/ramda-structures/rollup.config.js
+++ packages/ramda-structures/rollup.config.js
@@ -10,17 +10,6 @@ const config = {
             file: pkg.main,
             sourceMaps: true,
             format: 'cjs'
-        },
-        {
-            file: pkg.module,
-            sourceMaps: true,
-            format: 'esm'
-        },
-        {
-            file: pkg.browser,
-            sourceMaps: true,
-            format: "umd",
-            name: "JuanRamdaStructures"
         }
     ],
     external: [ "ramda" ],
