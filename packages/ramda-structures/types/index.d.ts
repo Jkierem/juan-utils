6a277e712a99067b93bf973a1aebbc1cc255e964
jkierem <juanmiguelgomez8@gmail.com>
2020-08-06 14:29:38 -0500

    Added effect method to strcutures

--- packages/ramda-structures/types/index.d.ts
+++ packages/ramda-structures/types/index.d.ts
@@ -9,7 +9,8 @@ declare module "@juan-utils/ramda-structures" {
     export interface None {
         match: (a: MaybeCases) => any;
         get: () => undefined;
-        map: (fn: (a: any) => any) => None;
+        map: (fn: (a: any) => any) => Maybe<any>;
+        effect: (fn: (a: any) => any) => Maybe<any>;
         chain: (fn: (a: any) => Maybe<any>) => Maybe<any>;
         equals: (b: any) => boolean;
         onNone: (fn: any) => any;
@@ -22,6 +23,7 @@ declare module "@juan-utils/ramda-structures" {
         match: (a: MaybeCases) => any;
         get: () => A;
         map: <B>(fn: (a:A) => B) => Just<B>;
+        effect: (fn: (a: any) => any) => Maybe<A>;
         chain: <B>(fn: (a:A) => Maybe<B>) => Maybe<B>;
         equals: (b: any) => boolean;
         onNone: (fn: any) => any;
@@ -59,26 +61,28 @@ declare module "@juan-utils/ramda-structures" {
     };
 
     export interface Ok<A> {
-        match: (cases: ResultCases) => any,
-        get: () => A,
-        map: <B>(fn: (a:A) => B) => Ok<B>,
-        chain: <B,C>(fn: (a:A) => Result<B,C>) => Result<B,C>,
-        equals: (b: any) => boolean,
-        onError: (fn: any) => any,
-        isOk: () => boolean,
-        isErr: () => boolean,
-    };
+        match: (cases: ResultCases) => any;
+        get: () => A;
+        map: <B>(fn: (a:A) => B) => Ok<B>;
+        effect: (fn: (a: any) => any) => Result<A,any>;
+        chain: <B,C>(fn: (a:A) => Result<B,C>) => Result<B,C>;
+        equals: (b: any) => boolean;
+        onError: (fn: any) => any;
+        isOk: () => boolean;
+        isErr: () => boolean;
+    }
 
     export interface Err<A> {
-        match: (cases: ResultCases) => any,
-        get: () => A,
-        map: <B>(fn: (a:A) => B) => Err<B>,
-        chain: <B,C>(fn: (a:A) => Result<B,C>) => Result<B,C>,
-        equals: (b: any) => boolean,
-        onError: (fn: any) => any,
-        isOk: () => boolean,
-        isErr: () => boolean,
-    };
+        match: (cases: ResultCases) => any;
+        get: () => A;
+        map: <B>(fn: (a:A) => B) => Err<B>;
+        effect: (fn: (a: any) => any) => Result<A,any>;
+        chain: <B,C>(fn: (a:A) => Result<B,C>) => Result<B,C>;
+        equals: (b: any) => boolean;
+        onError: (fn: any) => any;
+        isOk: () => boolean;
+        isErr: () => boolean;
+    }
 
     export type Result<A,B> = Ok<A> | Err<B>;
 
@@ -97,7 +101,7 @@ declare module "@juan-utils/ramda-structures" {
             <T>(a: T, b: T): boolean;
             <T>(a: T): (b: T) => boolean;
         };
-    };
+    }
 
     export function Ok<T>(val: T): Ok<T>;
     export function Err<T>(err: T): Err<T>;
@@ -109,7 +113,8 @@ declare module "@juan-utils/ramda-structures" {
     export interface Success<A> {
         match: (cases:TryCases) => any;
         get: () => A
-        map: (fn: (a:A) => B) => Success<B>;
+        map: <B>(fn: (a:A) => B) => Try<B,any>;
+        effect: (fn: (a: any) => any) => Try<A,any>;
         chain: <B,C>(fn: (a:A) => Try<B,C>) => Try<B,C>;
         equals: (b: any) => boolean;
         catch: <B>(fn: (a: A) => B) => A;
@@ -121,7 +126,8 @@ declare module "@juan-utils/ramda-structures" {
     export interface Failure<A> {
         match: (cases:TryCases) => any;
         get: () => A
-        map: (fn: (a:A) => B) => Success<B>;
+        map: <B>(fn: (a:A) => B) => Try<B,any>;
+        effect: (fn: (a: any) => any) => Try<A,any>;
         chain: <B,C>(fn: (a:A) => Try<B,C>) => Try<B,C>;
         equals: (b: any) => boolean;
         catch: <B>(fn: (a: A) => B) => B;
